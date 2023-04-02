802bc7a186c28c128532363d361f0d816b4afda0
INFORMICA\lukasz.birski <lukasz.birski@informica.pl>
2023-04-02 20:11:39 +0200

    Recipe Network Entity

---
+++ app/src/main/java/pl/birski/mvvmrecipeapp/network/model/RecipeResponse.kt
@@ -0,0 +1,32 @@
+package pl.birski.mvvmrecipeapp.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RecipeResponse(
+    @SerializedName("pk")
+    val pk: Int?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("publisher")
+    val publisher: String?,
+    @SerializedName("featured_image")
+    val featuredImage: String?,
+    @SerializedName("rating")
+    val rating: Int?,
+    @SerializedName("source_url")
+    val sourceUrl: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("cooking_instructions")
+    val cookingInstructions: String?,
+    @SerializedName("ingredients")
+    val ingredients: List<String>?,
+    @SerializedName("date_added")
+    val dateAdded: String?,
+    @SerializedName("date_updated")
+    val dateUpdated: String?,
+    @SerializedName("long_date_added")
+    val longDateAdded: Long?,
+    @SerializedName("long_date_updated")
+    val longDateUpdated: Long?
+)
